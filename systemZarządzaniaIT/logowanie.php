2fb8c9f7faa2fdb89eaee1f6905f427b1fa52c6b
FZ2161 <filipzalewski44@gmail.com>
2024-03-20 15:21:55 +0100

    logowanie wstępnie

--- systemZarządzaniaIT/logowanie.php
+++ systemZarządzaniaIT/logowanie.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style1.css">
     <title>logowanie</title>
 </head>
 <body>
@@ -20,7 +20,42 @@
     </form>
 
     <?php
+    if(!empty($_POST["login"])&&!empty($_POST["password"])){
+        $login=$_POST["login"];
+        $pass=$_POST["password"];
 
+        function szyfruj_haslo($pass){
+            return md5($pass);
+        }
+        $zaszyfrowane = szyfruj_haslo($pass);
+        
+        $dbHost="localhost";
+        $dbUser="root";
+        $dbPassword="";
+        $db="system_zarzadzania_it";
+
+        $conn=mysqli_connect($dbHost, $dbUser, $dbPassword, $db);
+
+        if(!$conn){
+            echo "err: " . mysqli_connect_error($conn);
+        }
+
+        $sql="SELECT * FROM users WHERE login='$login' && password='$zaszyfrowane'";
+
+        $results=mysqli_query($conn, $sql);
+
+        if(mysqli_num_rows($results)>0){
+            while($row=mysqli_fetch_assoc($results)){
+            $zalogowanoJako=$row["uprawnienia"];  //zapis zmiennej zalogowano jako:
+            echo "zalogowano jako: ". $row["uprawnienia"];
+        }
+        } else{
+            echo "niepoprawne dane";
+        }
+
+    } else {
+        echo "podaj wszystkie dane";
+    }
     ?>
 
 </body>
--- systemZarządzaniaIT/rejestracja.php
+++ systemZarządzaniaIT/rejestracja.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style1.css">
     <title>rejestracja</title>
 </head>
 <body>
@@ -38,7 +38,7 @@
         $conn=mysqli_connect($dbHost, $dbUser, $dbPassword, $db);
 
         if(!$conn){
-            // echo mysqli_connect_error($conn);
+            echo mysqli_connect_error($conn);
         }
 
         $sql="INSERT INTO users VALUES ('$login', '$zaszyfrowane', 'user')";
